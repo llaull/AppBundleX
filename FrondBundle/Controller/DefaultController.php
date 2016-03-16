72e2ce773e8b64fc1b2d68a4d46c43d9c929a9f8
llaull <laul@llovem.eu>
2016-03-16 16:06:45 +0100

    Signed-off-by: llaull <laul@llovem.eu>

--- FrondBundle/Controller/DefaultController.php
+++ FrondBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrondOfficeBundle:Default:index.html.twig');
+        return $this->render('FrondOfficeBundle:Default:metronic.html.twig');
     }
 }
