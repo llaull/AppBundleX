51b459c40afb391607a800f06986756f07e9d85b
llaull <laul@llovem.eu>
2016-08-04 16:52:00 +0200

    nouveau test pour le changement de pwd

---
+++ BackBundle/Tests/ChangePwdFormTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\BackBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ChangePwdFormTest extends WebTestCase
+{
+
+    public function testIndex()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+        $container = $kernel->getContainer();
+
+        $client = static::createClient();
+        $s = $container->get('back_office.loging');
+        $s->logIn($client);
+
+        // Create a new entry in the database
+        $crawler = $client->request('GET', '/profile/change-password');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /profile/edit");
+
+        $form = $crawler->selectButton('Sauvegarder')->form(array(
+            'fos_user_change_password_form[current_password]'  => 'test',
+            'fos_user_change_password_form[plainPassword][first]'  => 'test',
+            'fos_user_change_password_form[plainPassword][second]'  => 'test',
+        ));
+
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+
+        // Check data in the show view
+        $this->assertGreaterThan(0, $crawler->filter('div:contains("plup@gmail.com")')->count(), 'Missing element td:contains("test")');
+
+    }
+}
