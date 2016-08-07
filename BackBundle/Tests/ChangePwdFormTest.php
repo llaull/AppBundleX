927c0180e5dcf615ad9f599bac049fb291a07a48
llaull <laul@llovem.eu>
2016-08-07 08:51:55 +0200

    fix - test - contenu attendu

--- BackBundle/Tests/ChangePwdFormTest.php
+++ BackBundle/Tests/ChangePwdFormTest.php
@@ -32,7 +32,7 @@ class ChangePwdFormTest extends WebTestCase
 
 
         // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('div:contains("email@gmail.com")')->count(), 'Missing element td:contains("email@gmail.com")');
+        $this->assertGreaterThan(0, $crawler->filter('div:contains("test")')->count(), 'Missing element td:contains("test")');
 
     }
 }
